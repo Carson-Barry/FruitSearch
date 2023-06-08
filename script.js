07f1d33352f20ce10e2ef3e271bdeca0621084f6
Carson Barry <carsonbarry@Carsons-MacBook-Air.local>
2023-06-08 16:18:27 -0500

    completed search function

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
+	
+	const searchStr = str.toLowerCase()
 
-	// TODO
+	// I would like there to be some order to how the results appear, so I decided on the following:
 
-	return results;
+	// Get results with matches at the beginning of the string first
+	const first = fruit.filter(val => val.toLowerCase().startsWith(str))
+
+	// Next get results with matches at the beginning of a word within the string (if not already in first)
+	const second = fruit.filter(val => (val.toLowerCase().includes(" " + str.toLowerCase()) && !first.includes(val)));
+
+	// Finally get results with matches anywhere within the string (if not already in first or second)
+	const third = fruit.filter(val => (val.toLowerCase().includes(str.toLowerCase()) && !first.includes(val) && !second.includes(val)));
+
+	return [...first, ...second, ...third];
 }
 
 function searchHandler(e) {
-	// TODO
+	console.log(search(e.target.value));
+
 }
 
 function showSuggestions(results, inputVal) {
